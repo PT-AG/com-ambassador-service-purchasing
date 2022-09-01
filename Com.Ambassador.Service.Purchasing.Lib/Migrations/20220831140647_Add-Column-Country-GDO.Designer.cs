d6e408c954a68dc2ac5ca80359120aa14d7fadb0
AndreaZain <andrea.kinan@gmail.com>
2022-09-01 09:33:07 +0700

    Enhance - Garment Delivery Order
    Add Country

--- Com.Ambassador.Service.Purchasing.Lib/Migrations/20220831140647_Add-Column-Country-GDO.Designer.cs
+++ Com.Ambassador.Service.Purchasing.Lib/Migrations/20220831140647_Add-Column-Country-GDO.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Com.Ambassador.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220831140647_Add-Column-Country-GDO")]
+    partial class AddColumnCountryGDO
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
