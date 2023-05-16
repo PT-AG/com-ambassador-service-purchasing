aa9361f25e93f0243f51f0fde3de9dad4cfc9a95
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-05-16 10:42:56 +0700

    Enhance-InventoryRacking

---
+++ Com.Ambassador.Service.Purchasing.Lib/ViewModels/GarmentUnitReceiptNoteViewModels/DOItems/DOItemsRackingViewModels.cs
@@ -0,0 +1,14 @@
+﻿using Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitReceiptNoteViewModels.DOItems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitReceiptNoteViewModels
+{
+    public class DOItemsRackingViewModels
+    {
+        public string POSerialNumber { get; set; }
+        public List<RackingViewModels> Items { get; set; }
+
+    }
+}
