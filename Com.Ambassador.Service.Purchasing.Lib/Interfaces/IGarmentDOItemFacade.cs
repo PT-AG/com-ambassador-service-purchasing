b19b9e57648436357c79d9f99f20040350a55201
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-05-17 11:13:53 +0700

    Update DOitems Controller

--- Com.Ambassador.Service.Purchasing.Lib/Interfaces/IGarmentDOItemFacade.cs
+++ Com.Ambassador.Service.Purchasing.Lib/Interfaces/IGarmentDOItemFacade.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Com.Ambassador.Service.Purchasing.Lib.Models.GarmentUnitReceiptNoteModel;
+using Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitReceiptNoteViewModels;
+using Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitReceiptNoteViewModels.DOItems;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Com.Ambassador.Service.Purchasing.Lib.Interfaces
 {
@@ -6,5 +11,11 @@ namespace Com.Ambassador.Service.Purchasing.Lib.Interfaces
     {
         List<object> ReadForUnitDO(string Keyword = null, string Filter = "{}");
         List<object> ReadForUnitDOMore(string Keyword = null, string Filter = "{}", int size = 100);
+        List<DOItemsViewModels> GetByPO(string productcode, string po, string unitcode);
+        GarmentDOItems ReadById(int id);
+        Task<int> Update(int id, DOItemsRackingViewModels viewModels);
+        Task<List<StellingEndViewModels>> GetStellingQuery(int id, int offset);
+        MemoryStream GenerateExcel(string productcode, string po, string unitcode);
+        MemoryStream GeneratePdf(List<StellingEndViewModels> stellingEndViewModels);
     }
 }
