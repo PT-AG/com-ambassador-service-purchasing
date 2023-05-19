c368a9e57d4300696b10ffc90ff9666195ecb0fe
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-05-19 14:13:52 +0700

    update unit test

--- Com.Ambassador.Service.Purchasing.Test/Controllers/GarmentUnitReceiptNoteControllerTests/GarmentDOItemControllerTest.cs
+++ Com.Ambassador.Service.Purchasing.Test/Controllers/GarmentUnitReceiptNoteControllerTests/GarmentDOItemControllerTest.cs
@@ -1,13 +1,19 @@
-﻿using Com.Ambassador.Service.Purchasing.Lib.Interfaces;
+﻿using AutoMapper;
+using Com.Ambassador.Service.Purchasing.Lib.Interfaces;
+using Com.Ambassador.Service.Purchasing.Lib.Models.GarmentUnitReceiptNoteModel;
 using Com.Ambassador.Service.Purchasing.Lib.Services;
+using Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitReceiptNoteViewModels;
+using Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitReceiptNoteViewModels.DOItems;
 using Com.Ambassador.Service.Purchasing.WebApi.Controllers.v1.GarmentUnitReceiptNoteControllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Com.Ambassador.Service.Purchasing.Test.Controllers.GarmentUnitReceiptNoteControllerTests
@@ -102,6 +108,103 @@ namespace Com.Ambassador.Service.Purchasing.Test.Controllers.GarmentUnitReceiptN
 
             GarmentDOItemController controller = GetController(mockFacade);
             var response = controller.GetForUnitDOMore();
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+
+        [Fact]
+        public void GetDOItemsByPO_Success()
+        {
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.GetByPO(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new List<DOItemsViewModels>());
+
+            GarmentDOItemController controller = GetController(mockFacade);
+            var response = controller.GetDOItemsByPO("","","");
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+        [Fact]
+        public void GetDOItemsByPO_Error()
+        {
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.GetByPO(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                  .Throws(new Exception());
+
+            GarmentDOItemController controller = GetController(mockFacade);
+            var response = controller.GetDOItemsByPO("", "", "");
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+        [Fact]
+        public void GetXlsDOItemsByPO_Success()
+        {
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.GenerateExcel(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(new MemoryStream());
+
+            GarmentDOItemController controller = GetController(mockFacade);
+            var response = controller.GetXls("", "", "");
+            Assert.Equal("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", response.GetType().GetProperty("ContentType").GetValue(response, null));
+        }
+        [Fact]
+        public void GetXlsDOItemsByPO_Error()
+        {
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.GenerateExcel(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                  .Throws(new Exception());
+
+            GarmentDOItemController controller = GetController(mockFacade);
+            var response = controller.GetXls("", "", "");
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+        [Fact]
+        public void GetDOItemsByID_Success()
+        {
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.ReadById(It.IsAny<int>()))
+                .Returns(new GarmentDOItems());
+
+            GarmentDOItemController controller = GetController(mockFacade);
+            var response = controller.Get(0);
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+        [Fact]
+        public void GetDOItemsByID_Error()
+        {
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.ReadById(It.IsAny<int>()))
+                  .Throws(new Exception());
+
+            GarmentDOItemController controller = GetController(mockFacade);
+            var response = controller.Get(0);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+
+        [Fact]
+        public async Task Should_Success_Update_Data()
+        {
+        
+
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.Update(It.IsAny<int>(), It.IsAny<DOItemsRackingViewModels>()))
+               .ReturnsAsync(1);
+
+            var controller = GetController(mockFacade);
+
+            var response = await controller.Put(It.IsAny<int>(), new DOItemsRackingViewModels());
+            Assert.Equal((int)HttpStatusCode.NoContent, GetStatusCode(response));
+        }
+
+        [Fact]
+        public async Task Should_Error_Update_Data()
+        {
+
+
+            var mockFacade = new Mock<IGarmentDOItemFacade>();
+            mockFacade.Setup(x => x.Update(It.IsAny<int>(), It.IsAny<DOItemsRackingViewModels>()))
+                  .Throws(new Exception());
+
+            var controller = GetController(mockFacade);
+
+            var response = await controller.Put(It.IsAny<int>(), new DOItemsRackingViewModels());
             Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
         }
     }
