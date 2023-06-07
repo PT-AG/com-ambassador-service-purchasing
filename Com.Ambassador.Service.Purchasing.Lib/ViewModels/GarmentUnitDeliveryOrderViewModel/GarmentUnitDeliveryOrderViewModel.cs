186d2a1d2215fb3d66963ba89a46addf7c1e8b0f
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-06-07 13:31:14 +0700

    bugFix_Validation-Racking-UnitDOType
    -ignore racking for RETUR,SISA,MARKETING

--- Com.Ambassador.Service.Purchasing.Lib/ViewModels/GarmentUnitDeliveryOrderViewModel/GarmentUnitDeliveryOrderViewModel.cs
+++ Com.Ambassador.Service.Purchasing.Lib/ViewModels/GarmentUnitDeliveryOrderViewModel/GarmentUnitDeliveryOrderViewModel.cs
@@ -106,7 +106,7 @@ namespace Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitDeliveryOr
                 else
                 {
                     string itemError = "[";
-
+                    
                     foreach (var item in Items)
                     {
                         itemError += "{";
@@ -142,7 +142,7 @@ namespace Com.Ambassador.Service.Purchasing.Lib.ViewModels.GarmentUnitDeliveryOr
                                 }
                             }
 
-                            if (Storage.name == "GUDANG BAHAN BAKU" && UnitDOType != "SAMPLE" && UnitDOType != "RETUR" && (string.IsNullOrWhiteSpace(item.Colour) || string.IsNullOrWhiteSpace(item.Rack) || string.IsNullOrWhiteSpace(item.Box) || string.IsNullOrWhiteSpace(item.Level)))
+                            if (Storage.name == "GUDANG BAHAN BAKU" && UnitDOType != "RETUR" && UnitDOType != "MARKETING" && UnitDOType != "SISA" && (string.IsNullOrWhiteSpace(item.Colour) || string.IsNullOrWhiteSpace(item.Rack) || string.IsNullOrWhiteSpace(item.Box) || string.IsNullOrWhiteSpace(item.Level)))
                             {
                                 itemErrorCount++;
                                 itemError += $"Colour: 'Data Racking Belum Lengkap', ";
