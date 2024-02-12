61e5008f0d52b94a5a7943bf9106ef78f2f97dd1
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-02-12 13:51:21 +0700

    Enhance-AddProductSeries-GarmentDO

---
+++ Com.Ambassador.Service.Purchasing.Lib/Migrations/20240212045458_Add-Coloum-ProductSeries-GarmentDO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.Ambassador.Service.Purchasing.Lib.Migrations
+{
+    public partial class AddColoumProductSeriesGarmentDO : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductSeries",
+                table: "GarmentDeliveryOrderDetails",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductSeries",
+                table: "GarmentDeliveryOrderDetails");
+        }
+    }
+}
